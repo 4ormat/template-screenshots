1a971c2659b80d5fd669ebce7bc5c742e1bf8fcc
turbobeast <georgemichaelfaust@gmail.com>
2022-04-22 16:45:55 -0400

    Barebones of service * add package.json * add cloudinary SDK * add dotenv * trying out generating screenshot URLs

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+require('dotenv').config()
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const site_url = "https://discovernewborn1.zenfoliosite-test.zenfolio.dev/home";
+const options =  {
+  type: "url2png",
+  sign_url: true,
+  transform: [
+    { width: 1000, height: 1600 },
+  ]
+};
+
+const viewport_url = cloudinary.url(`${site_url}/url2png/viewport=1440x2400|fullpage=false|delay=12`, options);
+
+console.log(viewport_url);
